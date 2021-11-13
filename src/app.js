63c9bce3a171407e515898f48ce73d8e11f5739c
fatih <fsahinbas@gmail.com>
2021-11-13 14:21:53 +0300

    gun sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,13 @@
-console.log("Merhaba Kodlama.io")
+console.log('Merhaba Kodlama.io')
 
-//JS type safe değildir
-let dolarBugun = 9.30
+let dolarDun = 9.28
+let dolarBugun = 9.35
 
-let dolarDun = 9.20
-dolarDun = "9.20"
 {
-    let dolarDun = 9.10
+  let dolarDun = 9.11
 }
 
 console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
-
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
-}
-console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
+const euroDun = 11.25
+euroDun = 11.1
